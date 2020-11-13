5e162bd4d754aa985f2cc86cea8673e1c6fb321d
Mati Mutz <matias.mutz@gmail.com>
2020-11-13 16:52:16 -0300

    sdfsdf

---
+++ Kanban/Kanban/Pages/Estados/EstadoPage.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Kanban.Shared.Models;
+using Syncfusion.Blazor.Grids;
+using Syncfusion.Blazor.Navigations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Kanban.Pages.Modelos
+{
+    public class ModeloPageBase : ComponentBase
+    {
+        [Inject] protected CustomHttpClient Http { get; set; }
+        [Inject] protected IJSRuntime JsRuntime { get; set; }
+        protected SfGrid<Estado> Grid;
+
+        public bool Enabled = true;
+        public bool Disabled = false;
+
+        protected List<Estado> estados = new List<Estado>();
+        protected override async Task OnInitializedAsync()
+        {
+            estados = await Http.GetFromJsonAsync<List<Estado>>("api/Estado");
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
