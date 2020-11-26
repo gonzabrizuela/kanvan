b6e1aba925caaaf082722da7083f469a76b70ce5
Mati Mutz <matias.mutz@gmail.com>
2020-11-26 16:05:48 -0300

    asdf
    asdf

---
+++ Kanban/Kanban/Models/Programa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Kanban
+{
+    public class Programa
+    {
+        [Key]
+        public int REGISTRO { get; set; } = 0;
+        public int PEDIDO { get; set; } = 0;
+    }
+}
